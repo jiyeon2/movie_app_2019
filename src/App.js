ff0d49b72a1a80a2d5f7778ea764b5ee185e2197
jiyeon lee <a1919361@gmail.com>
2020-12-08 20:54:51 +0900

    ~#3.2 component life cycle

--- src/App.js
+++ src/App.js
@@ -38,7 +38,14 @@ class App extends React.Component {
     this.setState((current) => ({ count: current.count - 1 }));
     console.log("minus");
   };
+  componentDidMount() {
+    console.log("component rendered(mounted)");
+  }
+  componentDidUpdate() {
+    console.log("component updated");
+  }
   render() {
+    console.log("i am rendering");
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
