90e286ab7c469ad9f46e49c05a11f7a417e7ea79
jiyeon lee <a1919361@gmail.com>
2020-12-10 14:39:30 +0900

    detail 페이지 css추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -14,7 +15,29 @@ class Detail extends React.Component {
     // locations이 없으면(라우터 링크로 접근한 것이 아니면) 에러남
     const { location } = this.props;
     if (location.state) {
-      return <div>{location.state.title}</div>;
+      return (
+        <div className="detail">
+          <img
+            className="detail__image"
+            src={location.state.poster}
+            alt={`${location.state.title} poster`}
+            title={location.state.title}
+          />
+          <h2 className="detail__title">{location.state.title}</h2>
+          <div className="detail__year">{location.state.year}</div>
+          <div className="detail__genre">
+            {location.state.genres.map((genre, i) => {
+              return (
+                <span>
+                  {genre}
+                  {i === location.state.genres.length - 1 ? "" : ",  "}
+                </span>
+              );
+            })}
+          </div>
+          <div className="detail__summary">{location.state.summary}</div>
+        </div>
+      );
     } else {
       // location.state가 없으면 null리턴 후 componentDidMount실행되어 리다이렉션 발생
       return null;
